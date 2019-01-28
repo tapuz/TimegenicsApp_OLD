66763fbc9a95ec3204201061972bcf3e2359dfee
Thierry <thierry.duhameeuw@gmail.com>
2019-01-28 07:23:55 +0100

    v0.1

--- src/config.ts
+++ src/config.ts
@@ -2,4 +2,4 @@
 export const WORDPRESS_URL = 'https://dev.rugcentrumgent.be/alice/';
 export const WORDPRESS_REST_API_URL = WORDPRESS_URL + 'wp-json/wp/v2/';
 export const APM_API_URL = 'https://dev.rugcentrumgent.be/alice/pre/api.php';
-//export const APM_API_URL = 'http://192.168.0.2/alice/api.php';
+//export const APM_API_URL = 'http://192.168.0.2/alice/app/api.php';
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -100,7 +100,7 @@ export class HomePage {
   takePhoto(){
     //check if patientID was entered
     const options: CameraOptions = {
-      quality: 70,
+      quality: 50,
       destinationType: this.camera.DestinationType.DATA_URL,
       encodingType: this.camera.EncodingType.JPEG,
       mediaType: this.camera.MediaType.PICTURE,
