d621ed367a3f645b99f0010917d1f333748f4488
Thierry <thierry.duhameeuw@gmail.com>
2019-01-30 23:14:50 +0100

    color update

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,17 +2,7 @@ import { Component } from '@angular/core';
 import { NavController, LoadingController } from 'ionic-angular';
 import { PictureproofPage } from '../pictureproof/pictureproof';
 import { DocscanPage } from '../docscan/docscan';
-import { Camera, CameraOptions } from '@ionic-native/camera';
-import { FileTransfer, FileUploadOptions, FileTransferObject } from '@ionic-native/file-transfer';
-import { File } from '@ionic-native/file';
-import { AlertController } from 'ionic-angular';
-import * as Config from '../../config';
-import io from 'socket.io-client';
-import { WordpressService } from '../../services/wordpress.service';
 
-import { Http, Headers } from '@angular/http';
-
-import { Observable } from 'rxjs/Observable';
 
 
 @Component({
